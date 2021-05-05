8ed86badf9c113963e77e5002394a94a95eed62e
Rodrigo De Salvo Braz <rodrigobraz@fb.com>
2021-05-04 21:10:57 -0700

    - Allow multi-cell selection.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -60,7 +60,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     // Adding properties for drag and drop according
     // to https://doc.qt.io/qt-5/model-view-programming.html#using-model-view-classes
-    view->setSelectionMode(QAbstractItemView::SingleSelection);
+    view->setSelectionMode(QAbstractItemView::ExtendedSelection);
     view->setDragEnabled(true);
     view->viewport()->setAcceptDrops(true);
     view->setDropIndicatorShown(true);
